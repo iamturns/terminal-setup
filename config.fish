72662c6ccfd471fda2c73ef48137b2eaf7039c9a
Matt Turnbull <matt@iamturns.com>
2018-10-30 15:27:21 +1100

    feat: nvm support

--- config.fish
+++ config.fish
@@ -34,6 +34,32 @@ status --is-interactive; and . (rbenv init -|psub)
 thefuck --alias | source
 
 ###
+# nvm
+###
+
+nvm use default
+
+function __check_nvm --on-variable PWD --description 'Detect node version'
+  if test -f .nvmrc
+    set node_version (nvm version)
+    set nvmrc_node_version (nvm version (cat .nvmrc))
+
+    if [ $nvmrc_node_version = "N/A" ]
+      nvm install
+    else if [ $nvmrc_node_version != $node_version ]
+      nvm use
+    end
+
+    set -gx X_NVM_DIRTY_FLAG 1
+  else if set -q X_NVM_DIRTY_FLAG; and [ (node --version) != (nvm version default) ]
+    nvm use default
+    set -e X_NVM_DIRTY_FLAG
+  end
+end
+
+__check_nvm
+
+###
 # Theme (bobthefish)
 # https://github.com/oh-my-fish/theme-bobthefish
 ###
